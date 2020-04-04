51fb047e8bed65bf222953beb0def16152d40916
Michal <mickow95@gmail.com>
2020-04-04 18:36:28 +0200

    "dodanie wykladu"

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI 
+
+from pydantic import BaseModel
+
+app = FastAPI()
+@app.get('/')
+def hello_world():
+	return {"message": "Hello_world"}
+
+class HelloNameResp(BaseModel):
+	message: str
+
+@app.get('/hello/{name}', response_model=HelloNameResp)
+def hello_name(name: str):
+	#return {"message": f"Hello {name}"}
+	return HelloNameResp(message=f"Hello {name}")
