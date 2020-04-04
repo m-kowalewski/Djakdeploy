8a62431edf733c626ed55145243447ee415b0765
Michal <mickow95@gmail.com>
2020-04-04 21:57:41 +0200

    zad2

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ app = FastAPI()
 @app.get('/')
 def Hello():
 	return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def method():
+	return {"method":"GET"}
+
+@app.post("/method")
+def method():
+	return {"method":"POST"}
+
+@app.put("/method")
+def method():
+	return {"method":"PUT"}
+
+@app.delete("/method")
+def method():
+	return {"method":"DELETE"}
