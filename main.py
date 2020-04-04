64754d1ae51140e6fd78b93569e5725a8c4f34b7
Michal <mickow95@gmail.com>
2020-04-04 21:35:41 +0200

    zad1

--- main.py
+++ main.py
@@ -4,13 +4,5 @@ from pydantic import BaseModel
 
 app = FastAPI()
 @app.get('/')
-def hello_world():
-	return {"message": "Hello_world"}
-
-class HelloNameResp(BaseModel):
-	message: str
-
-@app.get('/hello/{name}', response_model=HelloNameResp)
-def hello_name(name: str):
-	#return {"message": f"Hello {name}"}
-	return HelloNameResp(message=f"Hello {name}")
+def Hello():
+	return {"message": "Hello World during the coronavirus pandemic!"}
